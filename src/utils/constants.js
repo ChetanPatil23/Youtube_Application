3ce3c07a8fce7e5ba7b3ce87b4a452addfe29a12
Chetan R. Patil <chetanrp@BLR.ALLEGISINDIA.COM>
2023-08-14 18:53:28 +0530

    Adding API key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = process.env.REACT_APP_API_KEY;
+const GOOGLE_API_KEY = "AIzaSyBFc6XSiLVc7fAcCNpmfdTHFytRIUd4qdw";
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
 export const YOUTUBE_SUGGESTIONS_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&key=${GOOGLE_API_KEY}&q=`;
 export const YOUTUBE_SEARCH_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
